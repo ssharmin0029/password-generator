21ba44d311820125fdf911228bcca61c180a6e29
ssharmin0029 <sabrina.sharmin29@myhunter.cuny.edu>
2021-02-19 23:19:02 -0500

    random password is generated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,8 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-
  // Define arrays to store characters to generate a random password
  var lowerCaseChar = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
  var upperCaseChar = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
  var numericChar = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
  var specialChar = ['!', '"-DOUBLE QUOTE', '#', '$', '%', '&', 'SINGLE QUOTE', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', 'Backslash', ']', '^', '_', '`', '{', '|', '}', '~'];
-
-//  var userInputPasswordLength = "";
-//  var confirmLowerCaseChar = boolean;
-//  var confirmUpperCaseChar = boolean;
-//  var confirmNumericChar = boolean;
-//  var confirmSpecialChar = boolean;
 
 // function definition generatePassword 
 function generatePassword () {
@@ -59,28 +37,46 @@ function generatePassword () {
   // var userLength = 
   // var confirmPasswod = confirmLowerCase.concat(confirmUpperCase);
   // Math.Floor(Math.Random() * length of the array confirmPasswrod) 
-  var userChoicePassword = [];
+  var userChoiceCharacterTypes = [];
   if (userChoiceLowerCaseChar === 'Y' || userChoiceLowerCaseChar === 'y') {
-    userChoicePassword = userChoicePassword.concat(lowerCaseChar);
+    userChoiceCharacterTypes = userChoiceCharacterTypes.concat(lowerCaseChar);
   }
 
   if (userChoiceUpperCaseChar === 'Y' || userChoiceUpperCaseChar === 'y') {
-    userChoicePassword = userChoicePassword.concat(upperCaseChar);
+    userChoiceCharacterTypes = userChoiceCharacterTypes.concat(upperCaseChar);
   }
 
   if (userChoiceNumericChar === 'Y' || userChoiceNumericChar === 'y') {
-    userChoicePassword = userChoicePassword.concat(numericChar);
+    userChoiceCharacterTypes = userChoiceCharacterTypes.concat(numericChar);
   }
 
   if (userChoiceSpecialChar === 'Y' || userChoiceSpecialChar === 'y') {
-    userChoicePassword = userChoicePassword.concat(specialChar);
+    userChoiceCharacterTypes = userChoiceCharacterTypes.concat(specialChar);
   }
 
-  
-  console.log(userChoicePassword);
+  alert("Your Choice of character type(s): " + userChoiceCharacterTypes);
 
-  return;
+  var randomPassword = "";
+
+  for (var i = 0; i < userInputPasswordLength; i++) {
+    randomPassword = randomPassword + userChoiceCharacterTypes[Math.floor(Math.random() * userChoiceCharacterTypes.length)];
+  }
+
+  return randomPassword;
 }
 
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
 
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
  
