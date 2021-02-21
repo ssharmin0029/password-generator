5db7ee8c53456c4402cbecbf047389cbfedc8f41
ssharmin0029 <sabrina.sharmin29@myhunter.cuny.edu>
2021-02-20 22:42:46 -0500

    code optimized - a helper function added, toUpperCase() method use, cleaned up

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,16 @@
+ // Declare empty variables to store string values 
+ var userChoiceLowerCaseChar = '';
+ var userChoiceUpperCaseChar = '';
+ var userChoiceNumericChar = '';
+ var userChoiceSpecialChar = '';
+ 
  // Define arrays to store characters to generate a random password
  var lowerCaseChar = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
  var upperCaseChar = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
  var numericChar = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
  var specialChar = ['!', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '{', '|', '}', '~'];
 
- // function definition generatePassword 
+ // Function definition generatePassword 
  function generatePassword () {
    // Prompts the user for the length of the password (between 8-128 inclusive)
    var userInputPasswordLength = window.prompt("Enter a number to choose the length of your password!\n(A length Between 8-128 Characters)");
@@ -12,7 +18,7 @@
    // Validates if the user entered a numeric value within the specified parameter 
    while (!(userInputPasswordLength >= 8 && userInputPasswordLength <= 128)) {
      // If the user clicks the Cancel button (null) --> end function execution 
-     if(userInputPasswordLength === null) {
+     if (userInputPasswordLength === null) {
        return;
      } 
      // Alerts the user the entered value is empty
@@ -26,66 +32,70 @@
    // Notifies the user about the chosen password length 
    window.alert("Your password will be " + userInputPasswordLength + " characters long!");
    
-   // Prompts the user to select the character types 
-   var userChoiceLowerCaseChar = window.prompt("Enter: (Y / y) to Include or (N / n) to Exclude... \nLowercase Characters in your password!");
-   var userChoiceUpperCaseChar = window.prompt("Enter: (Y / y) to Include or (N / n) to Exclude... \nUppercase Characters in your password!");
-   var userChoiceNumericChar = window.prompt("Enter: (Y / y) to Include or (N / n) to Exclude... \nNumeric Characters in your password!");
-   var userChoiceSpecialChar = window.prompt("Enter: (Y / y) to Include or (N / n) to Exclude... \nSpecial Characters in your password!");
+   // Function defintion to prompt the user to select character types 
+   function promptUserForCharTypes () {
+     userChoiceLowerCaseChar = window.prompt("Enter: (Y) to Include or (N) to Exclude... \nLowercase Characters in your password!").toUpperCase();
+     userChoiceUpperCaseChar = window.prompt("Enter: (Y) to Include or (N) to Exclude... \nUppercase Characters in your password!").toUpperCase();
+     userChoiceNumericChar = window.prompt("Enter: (Y) to Include or (N) to Exclude... \nNumeric Characters in your password!").toUpperCase();
+     userChoiceSpecialChar = window.prompt("Enter: (Y) to Include or (N) to Exclude... \nSpecial Characters in your password!").toUpperCase();
+   }
 
-   // Validates if the user selected at least one character type 
-   while (!((userChoiceLowerCaseChar === 'Y' || userChoiceLowerCaseChar === 'y') || (userChoiceUpperCaseChar === 'Y' || userChoiceUpperCaseChar === 'y') || (userChoiceNumericChar === 'Y' || userChoiceNumericChar === 'y') || (userChoiceSpecialChar === 'Y' || userChoiceSpecialChar === 'y'))) {
-     // Alerts user to choose at least 1 character type
+   // Function call
+   promptUserForCharTypes();
+
+   // Validates if the user selected at least 1 character type 
+   while (userChoiceLowerCaseChar !== 'Y' && userChoiceUpperCaseChar !== 'Y' && userChoiceNumericChar !== 'Y' && userChoiceSpecialChar !== 'Y') {
+     // Alerts the user to choose at least 1 character type
      window.alert("You Must Choose (Y or y) to include at least 1 Character Type!");
-     var userChoiceLowerCaseChar = window.prompt("Enter: (Y / y) to Include or (N / n) to Exclude... \nLowercase Characters in your password!");
-     var userChoiceUpperCaseChar = window.prompt("Enter: (Y / y) to Include or (N / n) to Exclude... \nUppercase Characters in your password!");
-     var userChoiceNumericChar = window.prompt("Enter: (Y / y) to Include or (N / n) to Exclude... \nNumeric Characters in your password!");
-     var userChoiceSpecialChar = window.prompt("Enter: (Y / y) to Include or (N / n) to Exclude... \nSpecial Characters in your password!");
-    }
-    
-    // Declares an empty array to store character types chosen by the user 
-    var userChoiceCharacterTypes = [];
-
-    // Checks each user input and concats that corresponding array in the new array
-    if (userChoiceLowerCaseChar === 'Y' || userChoiceLowerCaseChar === 'y') {
-      userChoiceCharacterTypes = userChoiceCharacterTypes.concat(lowerCaseChar);
-    }
-
-    if (userChoiceUpperCaseChar === 'Y' || userChoiceUpperCaseChar === 'y') {
-      userChoiceCharacterTypes = userChoiceCharacterTypes.concat(upperCaseChar);
-    }
-
-    if (userChoiceNumericChar === 'Y' || userChoiceNumericChar === 'y') {
-      userChoiceCharacterTypes = userChoiceCharacterTypes.concat(numericChar);
-    }
-    
-    if (userChoiceSpecialChar === 'Y' || userChoiceSpecialChar === 'y') {
-      userChoiceCharacterTypes = userChoiceCharacterTypes.concat(specialChar);
-    }
-    
-    //-------------------------------------------------------------------------
-    // Declares an empty string to store characters from chosen type(s) 
-    var userChosenCharacters = "";
-
-    // Loops through the array and stores in the empty string 
-    for (var i = 0; i <= userChoiceCharacterTypes.length - 1; i++) {
-      userChosenCharacters = userChosenCharacters + ' ' + userChoiceCharacterTypes[i];
-    }
-
-    // Notifies the user with the list of characters 
-    window.alert("Your Choice of character type(s): " + userChosenCharacters);
-    //-------------------------------------------------------------------------
-    
-    // Declares an empty string to store the randomly generated password 
-    var randomPassword = "";
-
-    // Loops through the array userChoiceCharacterTypes, using the userInputPasswordLength
-    // Each time gets a random character from the array, and adds to the string randomPassword
-    for (var i = 0; i < userInputPasswordLength; i++) {
-      randomPassword = randomPassword + userChoiceCharacterTypes[Math.floor(Math.random() * userChoiceCharacterTypes.length)];
-    }
-
-    // Returns the value of randomPassword by ending the function execution 
-    return randomPassword;
+     
+     // Function call to prompt the user to select character types 
+     promptUserForCharTypes();
+   }
+   
+   // Declares an empty array to store character types chosen by the user 
+   var userChoiceCharTypes = [];
+   
+   // Checks each user input to see if the user choice is equal to 'Y'; if equal then concats that corresponding array to a new array 
+   if (userChoiceLowerCaseChar === 'Y') {
+     userChoiceCharTypes = userChoiceCharTypes.concat(lowerCaseChar);
+   }
+   
+   if (userChoiceUpperCaseChar === 'Y') {
+     userChoiceCharTypes = userChoiceCharTypes.concat(upperCaseChar);
+   }
+   
+   if (userChoiceNumericChar === 'Y') {
+     userChoiceCharTypes = userChoiceCharTypes.concat(numericChar);
+   }
+   
+   if (userChoiceSpecialChar === 'Y') {
+     userChoiceCharTypes = userChoiceCharTypes.concat(specialChar);
+   }
+   
+   //-------------------------------------------------------------------------
+   // Declares an empty string to store characters from chosen type(s) 
+   var userChosenCharacters = "";
+   
+   // Loops through the array and stores in the empty string 
+   for (var i = 0; i <= userChoiceCharTypes.length - 1; i++) {
+     userChosenCharacters = userChosenCharacters + ' ' + userChoiceCharTypes[i];
+   }
+   
+   // Notifies the user with the list of characters 
+   window.alert("Your Choice of character type(s): " + userChosenCharacters);
+   //-------------------------------------------------------------------------
+   
+   // Declares an empty string to store the randomly generated password 
+   var randomPassword = "";
+   
+   // Loops through the array userChoiceCharacterTypes, using the userInputPasswordLength
+   // Each time gets a random character from the array, and adds to the string randomPassword
+   for (var i = 0; i < userInputPasswordLength; i++) {
+     randomPassword = randomPassword + userChoiceCharTypes[Math.floor(Math.random() * userChoiceCharTypes.length)];
+   }
+   
+   // Returns the value of randomPassword by ending the function execution 
+   return randomPassword;
   }
   
 // Assignment Code
